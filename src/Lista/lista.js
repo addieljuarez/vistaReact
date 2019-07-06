afc4b944bf31d5d9ff6a438886f9bf69770dd530
Addiel <addielJuarez@gmail.com>
2019-07-05 19:38:27 -0500

    servicio en boton

--- src/Lista/lista.js
+++ src/Lista/lista.js
@@ -1,17 +1,3 @@
-/**
- * File: /Users/addielJuarez/Documents/developer/react/my-app/src/Lista/lista.js
- * Project: /Users/addielJuarez/Documents/developer/react/my-app
- * Created Date: Friday, July 5th 2019, 6:44:15 pm
- * Author: Addiel Juarez
- * -----
- * Last Modified: Fri Jul 05 2019
- * Modified By: Addiel Juarez
- * -----
- * Copyright (c) 2019 Addiel
- * ------------------------------------
- */
-
-
 import React from "react";
 import { thisExpression } from "@babel/types";
 
@@ -25,9 +11,11 @@ export default class Lista extends React.Component{
 
     }
 
-    
-
     componentDidMount(){
+        
+    }
+
+    muestraLista(){
         var thisGeneral=this;
         fetch('http://localhost:8888/crudPHP/index.php/API/getAllUsers',{
             method: 'GET'
@@ -42,14 +30,15 @@ export default class Lista extends React.Component{
         .catch(function(error){
             console.log(error)
         })
-
-
-
     }
 
     render(){
         return (<div>
             Algo {this.state.texto}
+            <br></br>
+            <button
+                onClick={this.muestraLista.bind(this)}
+            >muestra la lista</button>
         </div>)
     }
 }
